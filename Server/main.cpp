f24ff5d097594bc4f36a242bfb7886d1f62b638c
hex <hex@hex.com>
2017-05-09 15:31:38 +0800

    Edit Copley's config

--- Server/main.cpp
+++ Server/main.cpp
@@ -38,6 +38,10 @@ int main(int argc, char *argv[])
 	{
 		xml_address = "../../Server/Robot_Faulhaber.xml";
 	}
+	else if (std::string(argv[1]) == "CP")
+	{
+		xml_address = "../../Server/RobotCopley.xml";
+	}
 	else if (std::string(argv[1]) == "EL")
 	{
 		xml_address = "../../Server/Robot_Elmo.xml";
